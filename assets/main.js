905703a8306a05b1c71dc8cd3b65b2caa2934f58
Andres Moreno <amorenompls@gmail.com>
2017-07-09 15:39:58 -0500

    Refactor code to DRY

--- assets/main.js
+++ assets/main.js
@@ -9,17 +9,34 @@ $(document).ready(function(){
         }
     });
 
+    /* getData Remove child nodes for the previous search (if any) and
+       then query the wikipedia API for entries matching the search
+       term
+
+       Input: - term: the term for which we are searching Wikipedia
+       articles
+
+       Return: - None. This is an asynchronous function
+
+       Side Effects: - A list of 10 matching terms (if any) have been
+       appended to an unordered list in the HTML page. Otherwise we
+       show nothing.
+     */
+    function getData(term){
+        deleteChildNodesById("wikiLinks");
+        getEntries(term);
+    }
+
     /* if the users presses enter in the search box, then:
        - delete any child nodes from the list of search terms which
-       might be there from a previous search
+         might be there from a previous search
        - Populate the list with search terms/descriptions/links
      */
     $('#searchBox').keydown(function(e){
         if(e.keyCode == 13) {
             // to avoid multiple submissions
             $(this).attr("disabled", "disabled");
-            deleteChildNodesById("wikiLinks");
-            getQuote(this.value);
+            getData(this.value);
             $(this).removeAttr("disabled");
         }
     });
@@ -30,8 +47,7 @@ $(document).ready(function(){
        - Populate the list with search terms/descriptions/links
      */
     $("#searchIcon").on('click', function(){
-        deleteChildNodesById("wikiLinks");
-        getQuote($('#searchBox').val());
+        getData($('#searchBox').val());
     });
 
     // from Eloquent Javascript: build nodes
@@ -73,7 +89,7 @@ $(document).ready(function(){
         return quoteURL + term;
     }
 
-    /* getQuote:
+    /* getEntries:
 
        Get data from Wikipedia: terms, short explanations, URLs
 
@@ -92,14 +108,15 @@ $(document).ready(function(){
        - response[2] has a short description of the terms
        - response[3] has the web link
      */
-    function getQuote(term) {
+    function getEntries(term) {
         $.getJSON(buildURL(term), function(response) {
             for (var i = 0; i < response[1].length; i++){
+                // entry header and entry summary
                 var internal = elt("li",
                                    elt("h2", response[1][i]),
                                    elt("p", response[2][i]));
                 var myNode = elt("a", internal);
-                // web link opens in new tab with some styling
+                // add web link in new tab with some styling
                 myNode.href = response[3][i];
                 myNode.target="_blank";
                 myNode.classList.add("mb-5");
